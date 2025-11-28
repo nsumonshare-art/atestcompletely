e2c18259620ba175b6e8876e2e4be43d7b92061f
nsumonshare-art <nsumonshare@gmail.com>
2025-11-28 11:23:34 -0800

    beta4

--- src/App.jsx
+++ src/App.jsx
@@ -227,6 +227,10 @@ const MonToEngTranslator = () => {
             }
         }
       };
+      
+      console.log("GEMINI KEY:", GEMINI_API_KEY);
+      console.log("FIREBASE CONFIG:", FIREBASE_CONFIG);
+
 
       const response = await fetch(
         `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-09-2025:generateContent?key=${apiKey}`,
